9f9f5f4f1eb0400b986b89f87751a1f92960be5c
suzchi <156890439+graybellows@users.noreply.github.com>
2024-06-06 17:27:32 -0600

    Add topic cards on the landing page

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,6 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    console.log("HERE");
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
